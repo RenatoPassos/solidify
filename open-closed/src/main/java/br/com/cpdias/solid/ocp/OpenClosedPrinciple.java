8df80703a9218afa5ee0ebdd15fc27f62aa6ca3d
Carlos Pereira Dias <carlospdiase@gmail.com>
2023-04-09 20:32:32 -0300

    :recycle: Aplica o Princípio de Open/Closed no projeto.

--- open-closed/src/main/java/br/com/cpdias/solid/ocp/OpenClosedPrinciple.java
+++ open-closed/src/main/java/br/com/cpdias/solid/ocp/OpenClosedPrinciple.java
@@ -3,41 +3,44 @@ package br.com.cpdias.solid.ocp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-enum TipoDePagamento{
-    CARTAO_DEBITO, //Adiciona 2.00 para cobrir custos operacionais
-    PIX, // Desconta .50 centavos 
-    CHEQUE // Adiciona 13%  a título de risco.
+interface OperacaoDePagamento{
+    double cobrancaTotal(double valor);    
+}
+class CartaoDeDebito implements OperacaoDePagamento{
+    @Override
+    public double cobrancaTotal(double valor) {
+        return valor + 2.00;
+    }
+}
+
+class Pix implements OperacaoDePagamento{
+    @Override
+    public double cobrancaTotal(double valor) {
+        return valor - 0.5;
+    }
+}
+
+class Cheque implements OperacaoDePagamento{
+    @Override
+    public double cobrancaTotal(double valor) {
+        return valor + (valor * 0.13);
+    }
 }
 public class OpenClosedPrinciple {
     private static final Logger LOGGER = LoggerFactory.getLogger(OpenClosedPrinciple.class);
     
-    
-    double cobrancaTotal(TipoDePagamento tipo, float valor) {
-        if (tipo == TipoDePagamento.CARTAO_DEBITO) {
-            return valor + 2f;
-        }
-        else if (tipo == TipoDePagamento.PIX) {
-            return valor - 0.5f;
-        }
-        else if (tipo == TipoDePagamento.CHEQUE) {
-            return valor + (valor * 0.13);
-        }
-        return 0f;
-    }
-    
     public static void main(String[] args) {
         LOGGER.info("Executando programa...");
         
-        OpenClosedPrinciple ocp = new OpenClosedPrinciple();
-        System.out.println(ocp.cobrancaTotal(TipoDePagamento.CARTAO_DEBITO, 100));
-        System.out.println(ocp.cobrancaTotal(TipoDePagamento.PIX, 100));
-        System.out.println(ocp.cobrancaTotal(TipoDePagamento.CHEQUE, 100));
+        OperacaoDePagamento debito = new CartaoDeDebito();
+        OperacaoDePagamento pix = new Pix();
+        OperacaoDePagamento  cheque = new Cheque();
+        
+        System.out.println(debito.cobrancaTotal(100));
+        System.out.println(pix.cobrancaTotal(100));
+        System.out.println(cheque.cobrancaTotal(100));
         
         LOGGER.info("Programa finalizado com sucesso!");
         
-    }
-    
-    
-    
+    }    
 }
-
