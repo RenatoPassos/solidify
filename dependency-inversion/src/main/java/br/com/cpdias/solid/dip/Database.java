897d87b3d313f4bb9e08cbb83e190cb3560570f8
Carlos Pereira Dias <carlos.dias@tse.jus.br>
2023-04-27 15:28:33 -0300

    :bug: Ajusta Exercício

--- dependency-inversion/src/main/java/br/com/cpdias/solid/dip/Database.java
+++
@@ -1,7 +0,0 @@
-package br.com.cpdias.solid.dip;
-
-// this is the abstract layer
-public interface Database {
-	public void connect();
-	public void disconnect();
-}
--- dependency-inversion/src/main/java/br/com/cpdias/solid/dip/MySQLDatabase.java
+++ dependency-inversion/src/main/java/br/com/cpdias/solid/dip/MySQLDatabase.java
@@ -1,13 +1,12 @@
 package br.com.cpdias.solid.dip;
 // low level module
-public class MySQLDatabase implements Database {
+public class MySQLDatabase {
 
-	@Override
+
 	public void connect() {
 		System.out.println("Conectando um banco MySQL...");
 	}
 	
-	@Override
 	public void disconnect() {
 		System.out.println("Desconectando o banco MySQL....");
 	}
